088ada0afbcd53af5ffa8095a2115e702e0d27cb
Luke Berndt <lukekb@gmail.com>
2021-11-23 07:27:39 -0500

    Update p25_parser.cc

--- trunk-recorder/systems/p25_parser.cc
+++ trunk-recorder/systems/p25_parser.cc
@@ -834,11 +834,12 @@ std::vector<TrunkMessage> P25Parser::parse_message(gr::message::sptr msg) {
   int shift = s0 << 8;
   long nac = shift + s1;
   
+  /*
   if (s.length() <= 2) {
     BOOST_LOG_TRIVIAL(error) << "P25 Parse error, s: " << s << " s0: " << s0 << " s1: " << s1 << " shift: " << shift << " nac: " << nac << " type: " << type;
     messages.push_back(message);
     return messages;
-  } 
+  } */
 
   if (nac == 0xffff) {
     // # TDMA
